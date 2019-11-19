8ac07aa54164b82478d07dc3dcd95b7a25eeb823
cha-yulan <54050727+cha-yulan@users.noreply.github.com>
2019-11-18 22:21:37 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,12 @@
-
+##Start of function
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
+  inv <- NULL ##1st data 
   place <- function(y) {
-    x <<- y
+    x <<- y ##association for loop
     inv <<- NULL
   }
   get <- function() x
-  placeInverse <- function() inv <<- solve(x) #calculate the inverse matrix
+  placeInverse <- function() inv <<- solve(x) ##calculates the inverse matrix
   getInverse <- function() inv
   list(place = place,
        get = get,
